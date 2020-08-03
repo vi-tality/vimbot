295f6ad8bccb6a02c16d4ad9f8b5e01696ecf948
Gytis Ivaskevicius <gytis.ivaskevicius@insoft.lt>
2020-08-03 16:06:20 +0300

    Fix exception on .env file not found

--- src/main.rs
+++ src/main.rs
@@ -76,7 +76,7 @@ group!({
 fn main() {
     // This will load the environment variables located at `./.env`, relative to
     // the CWD. See `./.env.example` for an example on how to structure this.
-    kankyo::load().expect("Failed to load .env file");
+    kankyo::load();
 
     // Initialize the logger to use environment variables.
     //
